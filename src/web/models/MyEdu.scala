1857736fe4eb003102751e09144dc8bc5cd501f8
Jonas Arnfred <jonas@ifany.org>
2013-04-30 16:09:23 +0000

    Added MyEdu Model

---
+++ src/web/models/MyEdu.scala
@@ -0,0 +1,55 @@
+package web
+
+import paper._
+import net.liftweb.json._
+
+object MyEduModel {
+
+  // The main datastructure
+  private var A : Analyzer = Analyzer(Map.empty[String, Document])
+
+  // Json nodes and edges ready to be served
+  private var nodes : String = ""
+  private var edges : String = ""
+
+  // Json clusters ready to be served
+  private var clusters : Map[String, String] = Map.empty
+
+  // Implicit val for JSON conversion
+  private implicit val formats = DefaultFormats
+
+  // Must be called to initialize all data from disk
+  def init(path : String) : Unit = { 
+    A = Analyzer.fromCache(path).load
+  }
+
+  // Function for getting an abstract
+  def getNode(id : String) : Option[String] = A.get(id).flatMap { d => d.data match {
+      case c @ Course(_, _, _)      => Some(Serialization.write(c))
+      case _                        => None
+    }
+  }
+
+  // Function for getting a json of all the nodes
+  def getNodeIds : String = {
+    if (nodes == "") nodes = Serialization.write(A.docs.map { case (id,_) => id })
+    return nodes
+  }
+
+  // Function for getting a json of all the nodes
+  def getEdges : String = {
+    if (edges == "") edges = Serialization.write(A.graph.edges)
+    return edges
+  }
+
+  // Returns a cluster of size/type k
+  def getClusters(k : String) : String = {
+    if (!clusters.contains(k)) {
+
+      val cs = for ((id, d) <- A.docs if d.cluster.contains(k)) yield (id -> d.cluster(k))
+      clusters += (k -> Serialization.write(cs))
+    }
+
+    return clusters(k)
+  }
+}
